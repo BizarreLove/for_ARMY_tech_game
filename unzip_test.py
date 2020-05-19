a956e7c8735301c17517442cba01813d84f22e9d
BizarreLove <41328139+BizarreLove@users.noreply.github.com>
2020-05-19 10:06:39 +0800

    Add files via upload

--- unzip_test.py
+++ unzip_test.py
@@ -3,24 +3,29 @@ import os
 
 #def un_zipFiles(path):
 path = "./"
-files=os.listdir(path)
-print(files)
+
+
 while 1:
     try:
+        files=os.listdir(path)
         for file in files:
+            if file.endswith('.txt'):
+                #print(file)
+                sys.exit()
+
+
+        for file in files:
+            
+
             if file.endswith('.zip'):
                 filePath=path+file
                 print(filePath)
                 zip_file = zipfile.ZipFile(filePath)
                 for names in zip_file.namelist():
-                    print(names)
-                    path = "./outfolder"
+                    #print(names)
                     zip_file.extract(names,path)
                 zip_file.close()
+
+
     except:
         break
-#name = "./out.zip"
-
-#un_zipFiles(name)
-
-
